b5022ca2886af01ca8a04c1588cde048246d0556
Mehdi Gholami <m3hdigholami@aut.ac.ir>
2024-07-19 07:08:22 +0330

    updated

--- today.py
+++ today.py
@@ -7,8 +7,11 @@ import time
 import hashlib
 
 # Personal access token with permissions: read:enterprise, read:org, read:repo_hook, read:user, repo
-HEADERS = {'authorization': 'token '+ os.environ['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']}
-USER_NAME = os.environ['m3hdi2gh']
+# my token: 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
+HEADERS = {'authorization': 'token '+ os.environ['ACCESS_TOKEN']}
+
+USER_NAME = os.environ['USER_NAME']
+
 QUERY_COUNT = {'user_getter': 0, 'follower_getter': 0, 'graph_repos_stars': 0, 'recursive_loc': 0, 'graph_commits': 0,
                'loc_query': 0}
 
